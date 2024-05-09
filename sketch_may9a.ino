29d91e7a6eb0d82e97c29f5473ad5f31050463b1
nv_$ <105254843+NVDnguyen@users.noreply.github.com>
2024-05-10 01:38:33 +0700

    Update sketch_may9a.ino

--- sketch_may9a.ino
+++ sketch_may9a.ino
@@ -8,7 +8,6 @@ HardwareSerial pointA(1);
 HardwareSerial pointB(2);
 
 void setup() {
-  // put your setup code here, to run once:
   pointA.begin(9600, SERIAL_8N1, 3, 1);
   pointB.begin(9600, SERIAL_8N1, 16, 17);
   Serial.begin(9600);
@@ -21,23 +20,20 @@ void loop() {
   delay(10);
 }
 void send() {
-  unsigned long currentMillis = millis();  // lấy thời gian hiện tại
-
-  // kiểm tra xem đã đủ thời gian chưa để in ký tự
+  unsigned long currentMillis = millis(); 
   if (currentMillis - previousMillis >= interval) {
-    // lưu lại thời gian hiện tại để kiểm tra trong lần gọi tiếp theo
+   
     previousMillis = currentMillis;
 
-    // in một ký tự (ví dụ: 'A')
+    // send sample data
     pointB.println("Ax");
     Serial.println("Send data: Ax");
   }
 }
 void receive() {
   while (pointA.available() > 0) {
-    // Đọc dữ liệu đến khi hết dữ liệu trong bộ nhớ đệm
-    String data = pointA.readStringUntil('x');  // Đọc đến khi gặp ký tự xuống dòng ('\n')
-    // In dữ liệu đã nhận được
+    String data = pointA.readStringUntil('x'); 
+    //print received data
     Serial.print("Received data: ");
     Serial.println(data);
   }
